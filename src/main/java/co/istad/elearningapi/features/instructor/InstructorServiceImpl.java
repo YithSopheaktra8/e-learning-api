538e8ccd0e75c270488268673831c066fb7b7c90
tevynhoem <tevymomnh193@gmail.com>
2024-04-17 18:33:32 +0700

    instructor

--- src/main/java/co/istad/elearningapi/features/instructor/InstructorServiceImpl.java
+++ src/main/java/co/istad/elearningapi/features/instructor/InstructorServiceImpl.java
@@ -3,15 +3,14 @@ package co.istad.elearningapi.features.instructor;
 import co.istad.elearningapi.domain.Instructor;
 import co.istad.elearningapi.features.instructor.dto.InstructorCreateRequest;
 import co.istad.elearningapi.features.instructor.dto.InstructorResponse;
+import co.istad.elearningapi.features.user.UserRepository;
 import co.istad.elearningapi.mapper.InstructorMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,20 +23,22 @@ public class InstructorServiceImpl implements InstructorService{
 
     private final InstructorRepository instructorRepository;
     private final InstructorMapper instructorMapper;
-    //private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public InstructorResponse createInstructor(InstructorCreateRequest instructorCreateRequest) {
-//        Instructor instructor = instructorMapper.instructorCreateRequestToInstructor(instructorCreateRequest, userRepository);
-//        Instructor savedInstructor = instructorRepository.save(instructor);
-//        return instructorMapper.instructorToInstructorResponse(savedInstructor);
-        return null;
+
+        Instructor instructor = instructorMapper.instructorCreateRequestToInstructor(instructorCreateRequest, userRepository);
+        Instructor savedInstructor = instructorRepository.save(instructor);
+        return instructorMapper.instructorToInstructorResponse(savedInstructor);
     }
 
     @Override
     public List<InstructorResponse> findAllInstructors(int page, int size) {
+
         Pageable pageable = PageRequest.of(page, size);
         Page<Instructor> instructorPage = instructorRepository.findAll(pageable);
+
         return instructorPage.stream()
                 .map(instructorMapper::instructorToInstructorResponse)
                 .collect(Collectors.toList());
@@ -45,19 +46,25 @@ public class InstructorServiceImpl implements InstructorService{
 
     @Override
     public InstructorResponse updateInstructor(String username, InstructorCreateRequest instructorCreateRequest) {
-//        Instructor instructor = instructorRepository.findInstructorByUserUserName(username)
-//                .orElseThrow(() -> new RuntimeException("Instructor not found with username: " + username));
-//        Instructor updatedInstructor = instructorMapper.instructorCreateRequestToInstructor(instructorCreateRequest, userRepository);
-//        updatedInstructor.setId(instructor.getId());
-//        updatedInstructor = instructorRepository.save(updatedInstructor);
-        //return instructorMapper.instructorToInstructorResponse(updatedInstructor);
-        return null;
+
+        Instructor instructor = instructorRepository.findInstructorByUserUserName(username)
+                .orElseThrow(() -> new RuntimeException(
+                        "Instructor not found with username: " + username));
+
+        Instructor updatedInstructor = instructorMapper.instructorCreateRequestToInstructor(instructorCreateRequest, userRepository);
+        updatedInstructor.setId(instructor.getId());
+        updatedInstructor = instructorRepository.save(updatedInstructor);
+
+        return instructorMapper.instructorToInstructorResponse(updatedInstructor);
     }
 
     @Override
     public InstructorResponse findInstructorByUsername(String username) {
+
         Instructor instructor = instructorRepository.findInstructorByUserUserName(username)
-                .orElseThrow(() -> new RuntimeException("Instructor not found with username: " + username));
+                .orElseThrow(() -> new RuntimeException(
+                        "Instructor not found with username: " + username));
+
         return instructorMapper.instructorToInstructorResponse(instructor);
     }
 }
--- src/main/java/co/istad/elearningapi/mapper/InstructorMapper.java
+++ src/main/java/co/istad/elearningapi/mapper/InstructorMapper.java
@@ -3,6 +3,7 @@ package co.istad.elearningapi.mapper;
 import co.istad.elearningapi.domain.Instructor;
 import co.istad.elearningapi.features.instructor.dto.InstructorCreateRequest;
 import co.istad.elearningapi.features.instructor.dto.InstructorResponse;
+import co.istad.elearningapi.features.user.UserRepository;
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -13,7 +14,7 @@ public interface InstructorMapper {
 
     InstructorResponse instructorToInstructorResponse(Instructor instructor);
 
-    //@Mapping(target = "user", source = "userId", qualifiedByName = "mapUserIdToUser")
-    //Instructor instructorCreateRequestToInstructor(InstructorCreateRequest instructorCreateRequest, @Context UserRepository userRepository);
+
+    Instructor instructorCreateRequestToInstructor(InstructorCreateRequest instructorCreateRequest, @Context UserRepository userRepository);
 
 }
